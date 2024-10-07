148f3495e22e145d76165d8977b29537deabd4c2
Mansur Isaev <737dab2f169a@mail.ru>
2024-10-07 08:49:20 +0400

    Add table export to file

--- scripts/database_editor.gd
+++ scripts/database_editor.gd
@@ -15,12 +15,14 @@ const RecordRenameDialog: GDScript = preload("res://scripts/record_rename_dialog
 const TableCreateDialog: GDScript = preload("res://scripts/table_create_dialog.gd")
 const TableDeleteDialog: GDScript = preload("res://scripts/table_delete_dialog.gd")
 const TableEditorDialog: GDScript = preload("res://scripts/table_editor_dialog.gd")
+const TableExportDialog: GDScript = preload("res://scripts/table_export_dialog.gd")
 const TableRenameDialog: GDScript = preload("res://scripts/table_rename_dialog.gd")
 
 
 enum TabContextMenu {
 	NEW,
 	EDIT,
+	EXPORT,
 	RENAME,
 	DELETE,
 }
@@ -39,6 +41,8 @@ var _table_view: TableView = null
 
 var _inspector_container: TabContainer = null
 var _inspector: Inspector = null
+
+var _table_export_dialog: TableExportDialog = null
 
 var _record_rename_dialog: RecordRenameDialog = null
 
@@ -178,6 +182,15 @@ func show_edit_table_dialog(table: Dictionary[StringName, Variant]) -> void:
 	table_editor.set_transient(true)
 	table_editor.set_transient_to_focused(true)
 
+func show_table_export_dialog(table: Dictionary[StringName, Variant]) -> void:
+	if is_instance_valid(_table_export_dialog):
+		_table_export_dialog.queue_free()
+
+	_table_export_dialog = TableExportDialog.new(table)
+	self.add_child(_table_export_dialog)
+
+	_table_export_dialog.popup_centered(Vector2i(500, 300))
+
 
 func show_rename_table_dialog(table: Dictionary[StringName, Variant]) -> void:
 	var rename_table: TableRenameDialog = TableRenameDialog.new(_database, table)
@@ -228,6 +241,8 @@ func _on_tab_rmb_clicked(tab_idx: int) -> void:
 	popup.add_separator()
 	popup.add_item("Edit Table", TabContextMenu.EDIT)
 	popup.add_separator()
+	popup.add_item("Export Table As...", TabContextMenu.EXPORT)
+	popup.add_separator()
 	popup.add_item("Rename Table", TabContextMenu.RENAME)
 	popup.add_item("Delete Table", TabContextMenu.DELETE)
 	popup.id_pressed.connect(func(option: TabContextMenu) -> void:
@@ -236,6 +251,8 @@ func _on_tab_rmb_clicked(tab_idx: int) -> void:
 				show_create_table_dialog()
 			TabContextMenu.EDIT:
 				show_edit_table_dialog(_tab_bar.get_tab_metadata(tab_idx))
+			TabContextMenu.EXPORT:
+				show_table_export_dialog(_tab_bar.get_tab_metadata(tab_idx))
 			TabContextMenu.RENAME:
 				show_rename_table_dialog(_tab_bar.get_tab_metadata(tab_idx))
 			TabContextMenu.DELETE:
