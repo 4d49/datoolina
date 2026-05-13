8939bdd64c5b520bd8305b3acfb906f42159f6b2
Mansur Isaev <737dab2f169a@mail.ru>
2026-05-13 10:58:17 +0400

    Split `FloatDataType` into `FloatRangeDataType` classes

--- scripts/database/impl/data_types/float_data_type.gd
+++ scripts/database/impl/data_types/float_data_type.gd
@@ -4,22 +4,11 @@
 extends AbstractDataType
 
 
-const MIN_VALUE: float = -3.4028235e+38
-const MAX_VALUE: float =  3.4028235e+38
-
-
-var _min_value: float = MIN_VALUE
-var _max_value: float = MAX_VALUE
-
-
 func validate(value: Variant) -> bool:
-	if value is float or value is int:
-		return value >= _min_value and value <= _max_value
-
-	return false
+	return value is float or value is int
 
 
-func get_default() -> Variant:
+func get_default() -> float:
 	return 0.0
 
 
@@ -27,19 +16,9 @@ func get_built_in_type() -> Variant.Type:
 	return TYPE_FLOAT
 
 
+func get_type_hint() -> Hint:
+	return Hint.NONE
+
+
 func get_type_name() -> StringName:
 	return &"float"
-
-
-func set_min_value(value: float) -> void:
-	_min_value = maxf(value, MIN_VALUE)
-
-func get_min_value() -> float:
-	return _min_value
-
-
-func set_max_value(value: float) -> void:
-	_max_value = minf(value, MAX_VALUE)
-
-func get_max_value() -> float:
-	return _max_value
---
+++ scripts/database/impl/data_types/float_range_data_type.gd
@@ -0,0 +1,45 @@
+# Copyright (c) 2024-2026 Mansur Isaev and contributors - MIT License
+# See `LICENSE.md` included in the source distribution for details.
+
+extends AbstractRangeDataType
+
+
+const MIN_VALUE: float = -3.4028235e+38
+const MAX_VALUE: float =  3.4028235e+38
+
+
+var _min_value: float = MIN_VALUE
+var _max_value: float = MAX_VALUE
+
+
+func set_min(value: Variant) -> void:
+	_min_value = maxf(value, MIN_VALUE)
+
+func get_min() -> float:
+	return _min_value
+
+
+func set_max(value: Variant) -> void:
+	_max_value = minf(value, MAX_VALUE)
+
+func get_max() -> float:
+	return _max_value
+
+
+func validate(value: Variant) -> bool:
+	if value is float or value is int:
+		return value >= get_min() and value <= get_max()
+
+	return false
+
+
+func get_default() -> float:
+	return 0.0
+
+
+func get_built_in_type() -> Variant.Type:
+	return TYPE_FLOAT
+
+
+func get_type_name() -> StringName:
+	return &"float_range"
