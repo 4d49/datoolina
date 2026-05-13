334fc2264ab32351248fbee328bb09f72b11b4ae
Mansur Isaev <737dab2f169a@mail.ru>
2026-05-13 11:05:16 +0400

    Update `BoolDataType` class

--- scripts/database/impl/data_types/bool_data_type.gd
+++ scripts/database/impl/data_types/bool_data_type.gd
@@ -5,15 +5,19 @@ extends AbstractDataType
 
 
 func validate(value: Variant) -> bool:
-	return value is bool
+	return value is bool or value is int
 
 
-func get_default() -> Variant:
+func get_default() -> bool:
 	return false
 
 
 func get_built_in_type() -> Variant.Type:
 	return TYPE_BOOL
+
+
+func get_type_hint() -> Hint:
+	return Hint.NONE
 
 
 func get_type_name() -> StringName:
