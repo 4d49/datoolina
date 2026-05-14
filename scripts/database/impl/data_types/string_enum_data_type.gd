cb6e01b1f9c7a1ad8fcdfa57fc7f0d651295fa13
Mansur Isaev <737dab2f169a@mail.ru>
2026-05-14 07:53:19 +0400

    Add `StringEnumDataType` class

---
+++ scripts/database/impl/data_types/string_enum_data_type.gd
@@ -0,0 +1,38 @@
+# Copyright (c) 2024-2026 Mansur Isaev and contributors - MIT License
+# See `LICENSE.md` included in the source distribution for details.
+
+extends AbstractEnumDataType
+
+
+var _options: Dictionary[StringName, StringName]
+var _option_map: Dictionary[StringName, StringName]
+
+var _default: StringName
+
+
+func get_options() -> Dictionary[StringName, StringName]:
+	return _options
+
+
+func get_enum_names() -> Array[StringName]:
+	return _options.keys()
+
+
+func validate(value: Variant) -> bool:
+	if _option_map.is_empty():
+		for name: StringName in _options:
+			_option_map[_options[name]] = name
+
+	return _option_map.has(value)
+
+
+func get_default() -> StringName:
+	return _default
+
+
+func get_built_in_type() -> Variant.Type:
+	return TYPE_STRING
+
+
+func get_type_name() -> StringName:
+	return "string_enum"
