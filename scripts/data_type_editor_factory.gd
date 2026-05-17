c8506195d898a3f3d93e05605e69e12803527ccf
Mansur Isaev <737dab2f169a@mail.ru>
2026-05-17 11:01:16 +0400

    Add `DataTypeEditorFactory` class

---
+++ scripts/data_type_editor_factory.gd
@@ -0,0 +1,36 @@
+
+@abstract
+class_name DataTypeEditorFactory
+extends Object
+
+
+static var _registry: Array[Dictionary] = []
+
+static func register(predicate: Callable, constructor: Callable) -> void:
+	var entry: Dictionary[StringName, Variant] = {
+		&"predicate": predicate,
+		&"constructor": constructor
+	}
+	entry.make_read_only()
+
+	_registry.push_back(entry)
+
+
+static func can_create(data_type: AbstractDataType) -> bool:
+	for entry: Dictionary in _registry:
+		var predicate: Callable = entry.predicate
+		if predicate.is_valid() and predicate.call(data_type):
+			return true
+
+	return false
+
+
+static func create_editor(data_type: AbstractDataType) -> AbstractDataTypeEditor:
+	for entry: Dictionary in _registry:
+		var constructor: Callable = entry.constructor
+		if not constructor.is_valid():
+			continue
+
+		return constructor.call(data_type)
+
+	return null
